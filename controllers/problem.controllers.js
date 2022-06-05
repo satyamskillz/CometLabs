b45cc01d02783a0ad7f58d5e4a0aebc3c69b38bd
Satyam sharma <satyamskillz@gmail.com>
2022-06-06 02:02:56 +0530

    V2.6.0 -  Created delete question controller

--- controllers/problem.controllers.js
+++ controllers/problem.controllers.js
@@ -3,6 +3,7 @@
 
 const sphereService = require("../services/sphere.service");
 const questionModel = require("../models/question.model");
+const testCaseModel = require("../models/testcase.model");
 const logger = require("../logger");
 /*
 	this controller used to add question by admin
@@ -53,6 +54,58 @@ module.exports.addQuestion = async (req, res) => {
 		logger.error(error);
 		return res.status(500).json({
 			message: "Failed to add question",
+			error: error,
+		});
+	}
+};
+
+/*
+    this controller used to delete question by admin
+    parameters: {
+        questionId: String,
+    }
+    response: {
+        message: String,
+    }
+*/
+
+module.exports.deleteQuestion = async (req, res) => {
+	try {
+		const { questionId } = req.params;
+
+		// check weather request body is vaild or not
+		if (!questionId) {
+			return res.status(400).json({
+				message: "questionId is required",
+			});
+		}
+
+		// get question from mongo database
+		const question = await questionModel.findById(questionId);
+
+		// delete question from sphere problem database
+		const { isDeleted, message } = await sphereService.deleteProblem(
+			question.problemId
+		);
+
+		if (!isDeleted) {
+			return res.status(400).json({
+				message: message,
+			});
+		}
+
+		// delete question from mongo database
+		await questionModel.findOneAndDelete({
+			_id: questionId,
+		});
+
+		return res.status(200).json({
+			message: "Question deleted successfull",
+		});
+	} catch (error) {
+		logger.error(error);
+		return res.status(500).json({
+			message: "Failed to delete question",
 			error: error,
 		});
 	}
