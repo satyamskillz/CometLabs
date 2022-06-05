55534626d654f0e6540257e7621ccbd36e2529e2
Satyam sharma <satyamskillz@gmail.com>
2022-06-05 20:34:10 +0530

    V1.6.0 - All role authentication middleware added

---
+++ middlewares/Authentication.js
@@ -0,0 +1,55 @@
+const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+
+// function to get token from authorization bearer header
+function getToken(req) {
+	if (
+		req.headers.authorization &&
+		req.headers.authorization.split(" ")[0] === "Bearer"
+	) {
+		return req.headers.authorization.split(" ")[1];
+	}
+	return null;
+}
+
+const Authentication = async (req, res, next) => {
+	// Get token from authorization header
+	// And check weather token is vaild or not
+	const token = getToken(req);
+	if (!token) {
+		return res.status(401).json({
+			message: "Authentication failed",
+		});
+	}
+
+	try {
+		// Verify the access token
+		const decoded = jwt.verify(token, process.env.JWT_SECRET_1);
+		req.user = decoded;
+
+		// Check if the user exists in the database
+		const user = await userModel.findOne({
+			_id: decoded.user,
+		});
+		if (!user) {
+			return res.status(401).json({
+				message: "Authentication failed",
+			});
+		}
+
+		// Check if the user has admin role or not
+		if (user.isAdmin) {
+			req.isAdmin = true;
+		} else {
+			req.isAdmin = false;
+		}
+	} catch (err) {
+		return res.status(401).json({
+			message: "Authentication failed",
+		});
+	}
+
+	next();
+};
+
+module.exports = Authentication;
