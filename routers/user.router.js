82692122f475447245acedb8e53eb9b1819e44da
Satyam sharma <satyamskillz@gmail.com>
2022-06-06 02:08:33 +0530

    V1.6.1 - Testing all role middleware

--- routers/user.router.js
+++ routers/user.router.js
@@ -1,9 +1,10 @@
 var userControllers = require("../controllers/user.controllers");
+var Authentication = require("../middlewares/Authentication");
 var express = require("express");
 var router = express.Router();
 
 // Testing user api
-router.get("/", userControllers.testController);
+router.get("/", Authentication, userControllers.testController);
 
 // Register user api
 router.post("/v1/register", userControllers.register);
