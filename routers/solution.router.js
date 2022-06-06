6ee631269897b3e11ccf3d65d44e39095b925c83
Satyam sharma <satyamskillz@gmail.com>
2022-06-06 17:13:32 +0530

    V3.3.0 - Added a API to submit solution

--- routers/solution.router.js
+++ routers/solution.router.js
@@ -1,9 +1,13 @@
+var solutionController = require("../controllers/solution.controllers");
+var Authentication = require("../middlewares/Authentication");
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-	res.render("index", { title: "Express" });
-});
+// this api is used to submit solution to sphere engine
+router.post(
+	"/v1/submit-solution",
+	Authentication,
+	solutionController.submitSolution
+);
 
 module.exports = router;
